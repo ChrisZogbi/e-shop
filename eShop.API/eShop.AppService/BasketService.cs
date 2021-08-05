9c8bffa35a5bcb2813045fa43c976b796dd1dfa1
ChrisZogbi <51448389+ChrisZogbi@users.noreply.github.com>
2021-08-05 23:55:59 +0200

    Logs

--- eShop.API/eShop.AppService/BasketService.cs
+++ eShop.API/eShop.AppService/BasketService.cs
@@ -2,6 +2,8 @@
 using eShop.AppService.Models;
 using eShop.AppService.ServiceInterfaces;
 using eShop.Domain.Aggregates;
+using Microsoft.Extensions.Logging;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -13,22 +15,31 @@ namespace eShop.AppService
     {
         private readonly IProductRepository _productRepository;
         private readonly IBasketRepository _basketRepository;
+        private readonly ILogger<BasketService> _logger;
 
-        public BasketService(IProductRepository productRepository, IBasketRepository basketRepository)
+        public BasketService(IProductRepository productRepository, IBasketRepository basketRepository, ILogger<BasketService> logger)
         {
             _productRepository = productRepository;
             _basketRepository = basketRepository;
+            _logger = logger;
         }
 
         public async Task AddItem(AddItemModel addItemModel)
         {
+
             var basket = await _basketRepository.GetUserBasket(addItemModel.UserId);
             var product = new Product();
 
             if (basket == null)
+            {
+                _logger.LogInformation("[BASKET]: The user's basket doesn't exist");
+
                 basket = await CreateBasket(addItemModel.UserId);
 
-            if (addItemModel.ProductId.HasValue)
+                _logger.LogInformation("[BASKET CREATED]: Created[<'{Date}'>], UserId : {UserId}", basket.CreationDate.ToShortDateString(), basket.UserId.ToString());
+            }
+
+            if (addItemModel.ProductId.HasValue && addItemModel.ProductId.Value > 0)
             {
                 product = await _productRepository.GetById(addItemModel.ProductId.Value);
             }
@@ -47,6 +58,10 @@ namespace eShop.AppService
             var basketItem = new BasketItem(basket.Id, product.Id, addItemModel.Quantity);
 
             await _basketRepository.AddItemToBasket(basketItem);
+
+            _logger.LogInformation("[ITEM ADDED TO SHOPPING CART]: Added[<'{Date}'>], UserId: {UserId}, ProductId: {ProductId}, Quantity: {Quantity}, Price[<€{Price}>", 
+                DateTime.UtcNow.ToShortDateString(), basket.UserId, basketItem.ProductId, basketItem.Quantity, basketItem.Product.Price);
+
         }
 
         public async Task<BasketSummaryModel> GetBasketSummary(int userId)
